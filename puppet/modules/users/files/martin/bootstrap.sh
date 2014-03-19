65db3baceb6ceff789bba452561cd7b543bd52e3
Martin Trojer <martin.trojer@gmail.com>
2014-03-19 19:37:15 +0000

    better bootstrap for martin

--- puppet/modules/users/files/martin/bootstrap.sh
+++ puppet/modules/users/files/martin/bootstrap.sh
@@ -1,7 +1,5 @@
 # bootstrap my private env
 
-git init
-git remote add origin git@github.com:martintrojer/dotfiles.git
-git pull origin master
-git submodule init
-git submodule update
+git clone git@github.com:martintrojer/dotfiles.git
+cd dotfiles
+./.bootstrap.sh -f
